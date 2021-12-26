9093cc7a41cf25aa2b3a19a12382e7169896e571
Kushal Shah <shahnepal98@gmail.com>
2021-12-26 10:33:40 +0545

    Python Code using Opencv

---
+++ drowsiness face detection.py
@@ -0,0 +1,115 @@
+# Importig text to speech conversion library
+import pyttsx3
+# Importing OpenCV Library for basic image processing functions
+import cv2
+# Numpy for array related functions
+import numpy as np
+# Dlib for deep learning based Modules and face landmark detection
+import dlib
+# face_utils for basic operations of conversion
+from imutils import face_utils
+
+# Initializing the camera and taking the instance
+cap = cv2.VideoCapture(0)
+# Initializing the face detector and landmark detector
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+# status marking for current state
+sleep = 0
+drowsy = 0
+active = 0
+status = ""
+color = (0, 0, 0)
+def call(msg):
+ engine = pyttsx3.init()
+ voices = engine.getProperty('voices')
+ engine.setProperty('voice', voices[0].id)
+ engine.setProperty('rate', 100)
+ engine.say(msg)
+ engine.runAndWait()
+# Using Ecludiean distance formula
+def compute(ptA, ptB):
+ dist = np.linalg.norm(ptA - ptB)
+ return dist
+def Mouth(x,y,a,b,c,d,e,f):
+ up1 = compute(y,f)+compute(a,e)+compute(b,d)
+ down1 = compute(x,c)
+ ratio1 = int(10 * (up1 / (2.0 * down1)))
+ return ratio1
+def blinked(a, b, c, d, e, f):
+
+ up = compute(b, d) + compute(c, e)
+ down = compute(a, f)
+ ratio = up / (2.0 * down)
+ # Checking if it is blinked
+ # print(ratio)
+ # print("------------")
+ if ratio > 0.23:
+  return 2
+ elif (0.15 < ratio <= 0.23):
+
+  return 1
+ else:
+  return 0
+while True:
+ _, frame = cap.read()
+ gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+ faces = detector(gray)
+ # detected face in faces array
+ for face in faces:
+
+  x1 = face.left()
+  y1 = face.top()
+  x2 = face.right()
+  y2 = face.bottom()
+  face_frame = copy.copy(frame)
+  cv2.rectangle(face_frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+  landmarks = predictor(gray, face)
+  landmarks = face_utils.shape_to_np(landmarks)
+ #The numbers are actually the landmarks which will show eye
+  left_blink = blinked(landmarks[36], landmarks[37],
+  landmarks[38], landmarks[41], landmarks[40],landmarks[39])
+  right_blink = blinked(landmarks[42], landmarks[43],
+  landmarks[44], landmarks[47], landmarks[46],landmarks[45])
+  mouth = Mouth(landmarks[60], landmarks[61], landmarks[62],landmarks[63],landmarks[64],landmarks[65], landmarks[66],landmarks[67])
+  print("left")
+  print(left_blink)
+  print("right")
+  print(right_blink)
+  print("mouth")
+  print(mouth)
+ # Now judge what to do for the eye blinks and mouth close or Open
+ if left_blink == 0 and right_blink == 0 and mouth < 1:
+  sleep += 1
+  drowsy = 0
+  active = 0
+ if sleep > 8:
+  status = "SLEEPING !!!"
+  color = (255, 0, 0)
+  call('Awake Up Sir')
+ elif left_blink == 1 or right_blink == 1 or mouth > 1:
+  sleep = 0
+  active = 0
+  drowsy += 1
+ if drowsy > 8:
+  status = "Drowsy !"
+  color = (0, 0, 255)
+  call('Drinking water and take fresh air')
+ else:
+  drowsy = 0
+  sleep = 0
+  active += 1
+ if active > 8:
+  status = "Active :)"
+  color = (0, 255, 0)
+ cv2.putText(frame, status, (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1.2,color, 3)
+ for n in range(0, 68):
+  (x, y) = landmarks[n]
+  cv2.circle(face_frame, (x, y), 1, (255, 255, 255), -1)
+  cv2.imshow("Result of detector", face_frame)
+  cv2.imshow("Frame", frame)
+  key = cv2.waitKey(1)
+ if key == 27:
+  break
+cap.release()
+cv2.destroyAllWindows()
